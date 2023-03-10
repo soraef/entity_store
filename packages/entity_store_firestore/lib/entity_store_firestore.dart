d854be371208f8689d426c48dcd7271a9ecf0992
soraef <sorasorasorayama@gmail.com>
2023-03-10 13:03:05 +0900

    export repository_interface

--- packages/entity_store_firestore/lib/entity_store_firestore.dart
+++ packages/entity_store_firestore/lib/entity_store_firestore.dart
@@ -6,3 +6,4 @@ export 'src/collection_type.dart';
 export 'src/firestore_repository/firestore_entity_notifier.dart';
 export 'src/firestore_repository.dart';
 export 'src/in_memory_repository.dart';
+export 'src/repository_interface.dart';
