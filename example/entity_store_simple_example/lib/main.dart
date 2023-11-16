3c6f41ae063ced3d4b215f7fdaa47a6ba6548593
soraef <sorasorasorayama@gmail.com>
2023-11-16 15:18:28 +0900

    Add Todo model and TodoRepository to entity_store_simple_example

--- example/entity_store_simple_example/lib/main.dart
+++ example/entity_store_simple_example/lib/main.dart
@@ -1,53 +1,40 @@
+import 'package:entity_store/entity_store.dart';
 import 'package:flutter/material.dart';
 
+import 'models/todo.dart';
+
+final entityStoreNotifier = EntityStoreNotifier();
+final entityStoreController = EntityStoreController(entityStoreNotifier);
+final storageHandler = InMemoryStorageHandler();
+final todoRepository = TodoRepository(entityStoreController, storageHandler);
+
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return EntityStoreProviderScope(
+      entityStoreNotifier: entityStoreNotifier,
+      child: MaterialApp(
+        title: 'Entity Store Simple Example',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(title: 'Simple Todo Example'),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -55,71 +42,64 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _id = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _addTodo() {
+    _id++;
+    todoRepository.save(Todo.create(_id));
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final todoIds = context.selectEntities<int, Todo, List<int>>(
+      (value) => value.ids.toList(),
+    );
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: ListView(
+        children: [
+          for (final todoId in todoIds)
+            TodoTile(
+              todoId: todoId,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _addTodo,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
+  }
+}
+
+class TodoTile extends StatelessWidget {
+  final int todoId;
+  const TodoTile({
+    super.key,
+    required this.todoId,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final todo = context.watchEntity<int, Todo>(todoId);
+    if (todo == null) {
+      return const SizedBox();
+    }
+
+    return CheckboxListTile(
+      title: Text(todo.name),
+      value: todo.isDone,
+      onChanged: (bool? value) {
+        if (value == null) {
+          return;
+        }
+        todoRepository.save(todo.toggle());
+      },
     );
   }
 }
---
+++ example/entity_store_simple_example/lib/models/todo.dart
@@ -0,0 +1,58 @@
+import 'package:entity_store/entity_store.dart';
+import 'dart:math';
+
+String getRandomTodoName() {
+  final random = Random();
+  const todoNames = [
+    "Conquer the Laundry Mountain",
+    "Tame the Wild Email Inbox",
+    "Embark on the Epic Dishwashing Quest",
+    "Navigate the Grocery Store Jungle",
+    "Master the Art of Sock Pairing"
+  ];
+
+  return todoNames[random.nextInt(todoNames.length)];
+}
+
+class Todo extends Entity<int> {
+  @override
+  final int id;
+  final String name;
+  final bool isDone;
+
+  Todo(this.id, this.name, this.isDone);
+
+  factory Todo.create(int id) {
+    return Todo(
+      id,
+      getRandomTodoName(),
+      false,
+    );
+  }
+
+  Todo toggle() {
+    return Todo(
+      id,
+      name,
+      isDone ? false : true,
+    );
+  }
+}
+
+class TodoRepository extends LocalStorageRepository<int, Todo> {
+  TodoRepository(super.controller, super.localStorageHandler);
+
+  @override
+  Todo fromJson(Map<String, dynamic> json) {
+    return Todo(json["id"], json["name"], json["isDone"]);
+  }
+
+  @override
+  Map<String, dynamic> toJson(Todo entity) {
+    return {
+      "id": entity.id,
+      "name": entity.name,
+      "isDone": entity.isDone,
+    };
+  }
+}
